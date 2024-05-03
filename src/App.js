fc52adefb7210bd290e4cc77460aad1ba9b284bb
blink1197 <cgbagotsay11@gmail.com>
2024-05-03 14:52:09 +0800

    Made the website reponsive hiding the sidebar if screen is small(mobile)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 
-import Header from "./components/Header";
 import Main from "./components/Main";
 
 function App() {
   return (
     <div className="flex flex-col w-full h-full App bg-slate-200 font-Outfit">
-      <Header />
       <Main />
     </div>
   );
